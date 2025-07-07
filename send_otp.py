14f9d5db9f733a09ab701b7e9188ccc4d2b0d42e
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-07-07 15:02:36 +0600

    update send_otp.py

--- send_otp.py
+++ send_otp.py
@@ -7,17 +7,17 @@ import secrets
 
 load_dotenv()
 
-email_body = f"""
-Your otp is {otp_code} to sign up for pyTest.
-If It's not you, You can simply ignore this email.
-Stay secure,
-pyTest by Muhammad Hamim Islam ©2025
-"""
 email_sender = os.getenv("EMAIL_SENDER")
 email_password = os.getenv("APP_PASSWORD")
 
 def send_otp(email_receiver):
     otp_code = "".join(str(secrets.randbelow(10)) for _ in range(6))
+    email_body = f"""
+Your otp is {otp_code} to sign up for pyTest.
+If It's not you, You can simply ignore this email.
+Stay secure,
+pyTest by Muhammad Hamim Islam ©2025
+"""
     # setup for sending email 
     em = EmailMessage()
     em["From"] = email_sender
